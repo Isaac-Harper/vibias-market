d11f883353a7507daa5a8efb885b55a9d8350ded
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-10-19 13:14:59 -0700

    toggle

--- src/lib/HorizontalList.svelte
+++ src/lib/HorizontalList.svelte
@@ -38,7 +38,7 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        margin: 0 .2rem;
+        margin: 0 .f2rem;
 
     }
 </style>
--- src/lib/MarketList.svelte
+++ src/lib/MarketList.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
 	import HorizontalList from "./HorizontalList.svelte";
-
+    import {current_market} from "$lib/db"
+    
     export let markets
+    export let active_market
+    
+    
+    function toggle_market(market) {
+        if ( market == active_market) {
+            currenr_market.set({})
+        } else {
+            current_market.set(market)
+        }
+    }
 </script>
 
 
@@ -9,7 +20,15 @@
 
 <div class="holder element--border--primary">
     <h3>Markets</h3>
-    <HorizontalList elements={markets}/>
+    
+    <div class="container">
+        {#each markets as element}
+            <div on:click={() => open_market(element)} class="item element--border--primary">
+                {element.name}
+            </div>
+    
+        {/each}
+    </div>
 </div>
 
 <style>
@@ -26,5 +45,24 @@
 
     h3 {
         padding: 0 .5rem;
+    }
+    
+    .container {
+        display: flex;
+        overflow-x: scroll;
+        padding: 0;
+        padding: 0 .3rem .5rem;
+
+        height: 100%;
+    }
+
+    .item {
+        height: 100%;
+        flex: 0 0 8rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 0 .f2rem;
+
     }
 </style>
