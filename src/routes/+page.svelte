fec8d0c1b623bbbe236d5d742870707ca5600148
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-10-19 17:28:24 -0700

    ahhhhh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,24 @@
-<script context="module" lang="ts">
-	import ItemHolder from '$lib/ItemHolder.svelte';
-	import ShopHolder from '$lib/ShopHolder.svelte';
-	import type {Shop_type} from '$lib/types/Shop_type';
-
-    import Auth from "$lib/Auth.svelte"
-
-    import { user, getMarkets, getShops, getItems, current_market_id, current_shop_id, current_markets, current_shops, current_items, patron_list, getPatrons } from '$lib/db'
-
-    import User from "$lib/User.svelte"
-	import MarketHolder from '$lib/MarketHolder.svelte';
-	import Character from '$lib/Character.svelte';
-
-
-    
-</script>
-
-
 <script lang="ts">
-    let active_market_id:number
-    let active_shop_id:number
+	import HorizontalList from "$lib/HorizontalList.svelte";
+	import ItemInfo from "$lib/ItemInfo.svelte";
+	import MarketInfo from "$lib/MarketInfo.svelte";
+	import MarketList from "$lib/MarketList.svelte";
+	import ShopInfo from "$lib/ShopInfo.svelte";
+	import UserInfo from "$lib/UserInfo.svelte";
+	import Auth from "$lib/Auth.svelte";
+	import { user, auth, getMarkets, getShops, getItems, current_market_id, current_shop_id, current_item_id, current_market, current_shop, current_item, patron_list, getPatrons, market_list, shop_list, item_list } from '$lib/db'
+    
+    $: innerHeight = 0
+    $: innerWidth = 0
 
     let markets:any[]
     let shops:Shop_type[]
     let items:any[]
     let patrons: any[]
+    
+    let active_market
+    let active_shop
+    let active_item
     
 
     export async function reload() {
@@ -31,25 +26,17 @@
         shops = await getShops()
         items = await getItems()
     }
-    
-    current_market_id.subscribe(value => {
-        active_market_id = value
-    })
-
-    current_shop_id.subscribe(value => {
-        active_shop_id = value
-    })
 
 
-    current_markets.subscribe(value => {
+    market_list.subscribe(value => {
         markets = value
     })
 
-    current_shops.subscribe(value => {
+    shop_list.subscribe(value => {
         shops = value
     })
 
-    current_items.subscribe(value => {
+    item_list.subscribe(value => {
         items = value
     })
 
@@ -57,42 +44,65 @@
         patrons = value
     })
 
+    current_market.subscribe(v => {
+        active_market = v
+    }) 
+    
+    current_shop.subscribe(v => {
+        active_shop = v
+    }) 
+    
+    current_item.subscribe(v => {
+        active_item = v
+    }) 
+
     async function init() {
-        current_markets.set(await getMarkets())
-        current_shops.set(await getShops())
-        current_items.set(await getItems())
+        market_list.set(await getMarkets())
+        shop_list.set(await getShops())
+        item_list.set(await getItems())
         patron_list.set(await getPatrons())
-
-        console.log(markets)
-        
-        let unique_markets_ids: number[] = []
-        markets.forEach(market => {
-            if (!unique_markets_ids.includes(market.id)) {
-                unique_markets_ids.push(market.id)
-            }
-        });
-
-        current_market_id.set(unique_markets_ids[0])    
+ 
     }
-
-    $: current_market = markets.filter(v => v.id === active_market_id)
-
 </script>
 
 
+<svelte:window bind:innerHeight bind:innerWidth/>
 
-{#if $user} 
-    <User/>
-    {#await init() then}
-        
 
-        <MarketHolder markets={markets}/>
-        <Character patrons={patrons} current_market={current_market}/>
-        <ShopHolder active_market_id={active_market_id} shops={shops} current_market={current_market}/>
-        <ItemHolder active_shop_id={active_shop_id} items={items} current_market={current_market}/>
-        
-    {/await}
+
+{#if $user}
+    <div class="grid" style="height: {innerHeight}px; width: {innerWidth}px;">
+        <UserInfo/>
+        {#await init() then}
+            <MarketList/>
+            <MarketInfo/>
+            <ShopInfo {items} {active_shop}/>
+            <ItemInfo {active_item}/>
+        {/await}
+    </div>
 {:else}
     <Auth/>
 {/if}
 
+
+<style>
+    .grid {
+        padding: 1rem;
+        overscroll-behavior: contain;
+        height: 100%;
+        width: 100;
+        display: grid;
+        gap: .5rem;
+        grid-template-rows: .5fr .5fr minmax(0, 2fr) minmax(0,6fr) minmax(0, 4fr) minmax(0, 2fr);
+        grid-template-columns: 5fr 2fr;
+
+        grid-template-areas: 
+            "user       logout"
+            "user       settings"
+            "marketL    marketL"
+            "marketI    marketI"
+            "shopI      shopI"
+            "itemI      itemI";
+    }
+
+</style>
