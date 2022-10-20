fb48e0275084eae06e30e3b232fe54e365e33958
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-10-19 17:30:46 -0700

    clean

--- src/lib/User.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { user, auth } from '$lib/db'
-</script>
-
-
-{#if $user}
-    <p>You are signed in as {$user.email}</p>
-    <button on:click={() => auth.signOut()}>Sign out</button>
-{/if}
