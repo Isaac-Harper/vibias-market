76ff97aa521a427979c22ca7a0f3b27ca245d77c
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-11-03 13:03:52 -0700

    shop sets

--- src/lib/SHOPS/ShopSettings.svelte
+++ src/lib/SHOPS/ShopSettings.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
       import {user, current_shop, updateShop} from "$lib/db"
+      import TextInput from "$lib/TextInput.svelte"
+      import TextArea from "$lib/TextArea.svelte"
+      
+      
 
 </script>
 
-<details>
-    <summary>
-        <h3>Shop Settings:</h3>
-    </summary>
-    settings here
-</details>
-
 
 {#if $current_shop.creator_id !== $user.id }
-
-    <details>
-        <summary>
-            <h3>Shop Settings:</h3>
-        </summary>
-        settings here
-    </details>
+      <details>
+            <summary>
+                  <h3>Shop Settings:</h3>
+            </summary>
+        
+        
+            <TextInput title="Shop Name" bind:value={$current_shop.name}></TextInput>
+            <TextArea title="Shop Description" bind:value={$current_shop.description}></TextArea>
+            <button on:click={() => updateShop($current_shop)}>Apply</button>
+      </details>
 
 {/if}
