f142bc958ad9f4ad919c52370f5f4f251f421601
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-11-09 11:49:27 -0700

    sets

--- src/lib/MARKETS/MarketSettings.svelte
+++ src/lib/MARKETS/MarketSettings.svelte
@@ -6,37 +6,47 @@
 	import NumberInput from "$lib/NumberInput.svelte";
 	import TextArea from "$lib/TextArea.svelte";
 	import TextInput from "$lib/TextInput.svelte";
+	
+	let setting_open = false
 
     $: new_market = $current_market
     $: markets_patrons = $patron_list.filter(v => v.market_id === $current_market.id)
+	
+	function toggleSettings() {
+		setting_open = !setting_open
+	}
 </script>
 
+
+
+
 {#if $user.id === $current_market.creator_id}
-    <div class="container">
-        <Dropdown>
-            <span slot="title">Market Setting</span>
-            <div slot="body" class="body">
-                <TextInput bind:value={new_market.name} title="Name"/>
-                <TextArea title="Description" bind:value={new_market.description}></TextArea>
-                <NumberInput title="New patrons starting gold:" bind:value={new_market.starting_coins}/>
-                <p>Your join ID is:</p>
-                <p style="font-size: small;">{new_market.join_id}</p>
-                {#each markets_patrons as patron}
-                    <div class="flex">
-                        {patron.name}
-                        {patron.coins}
-                        <button>Remove Player</button>
-                        <button>Change coins</button>
-                    </div>
-                {/each}
-                <div class="flex">
-                    <ApplyButton type="market" obj={new_market}/>
-                    <DeleteButton type="market" id={$current_market.id}/>
-                </div>
-                
-            </div>
-        </Dropdown>
-    </div>
+	{#if setting_open}
+		<div class="container">
+			<h3>Market Setting</h3>
+			<div class="body">
+				<TextInput bind:value={new_market.name} title="Name"/>
+				<TextArea title="Description" bind:value={new_market.description}></TextArea>
+				<NumberInput title="New patrons starting gold:" bind:value={new_market.starting_coins}/>
+				<p>Your join ID is:</p>
+				<p style="font-size: small;">{new_market.join_id}</p>
+				{#each markets_patrons as patron}
+					<div class="flex">
+						{patron.name}
+						{patron.coins}
+						<button>Remove Player</button>
+						<button>Change coins</button>
+					</div>
+				{/each}
+				<div class="flex">
+					<ApplyButton type="market" obj={new_market}/>
+					<DeleteButton type="market" id={$current_market.id}/>
+				</div>
+			</div>
+		</div>
+	{:else}
+		<button class="ri-settings-line" on:click={toggleSettings}></button>>
+	{/if}
 {/if}
 
 
