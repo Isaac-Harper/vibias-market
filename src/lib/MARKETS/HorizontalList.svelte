aa96ea020b1eff6f919b837453ec1ac77ea48f25
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-11-10 10:24:39 -0700

    market togs

--- src/lib/MARKETS/HorizontalList.svelte
+++ src/lib/MARKETS/HorizontalList.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { user, current_market, market_list, shop_list, item_list, patron_list, resetShop, resetMarket, resetPatron, current_patron, inventory, resetItem, current_item, current_shop, state} from "$lib/db"
+    import { resetState, openMarket, user, current_market, market_list, shop_list, item_list, patron_list, resetShop, resetMarket, resetPatron, current_patron, inventory, resetItem, current_item, current_shop, state} from "$lib/db"
 
     export let list = "none"
 	export let content = []
@@ -8,7 +8,7 @@
 	
     function toggle_market(market) {
         if ( market == $current_market) {
-			$state.market_open = false
+			resetState()
             resetMarket()
             resetPatron()
         } else {
@@ -21,7 +21,7 @@
                 resetPatron()
             }
             resetShop()
-			$state.market_open = true
+			openMarket()
         }
     }
 
