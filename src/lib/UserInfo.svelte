8da75d2c3b7401661e43f147c4818b3925a68b00
voldari <isaacharper@gmail.com>
2022-12-17 15:38:01 -0700

    fix breaking error, refrencing database object outside sync

--- src/lib/UserInfo.svelte
+++ src/lib/UserInfo.svelte
@@ -1,89 +1,74 @@
 <script>
-    import { user, auth } from '$lib/db'
+	import { user, auth } from '$lib/db';
 	import { text } from 'svelte/internal';
 	import Secondary from './Buttons/Secondary.svelte';
 
-    let path = "logo.png"
+	let path = 'logo.png';
 
-    function togglePath() {
-        if (path === "logo.png") {
-            path = "logo_back.png"
-        } else {
-            path = "logo.png"
-        }
-    }
+	function togglePath() {
+		if (path === 'logo.png') {
+			path = 'logo_back.png';
+		} else {
+			path = 'logo.png';
+		}
+	}
 </script>
 
+<div class="holder">
+	<img src={path} on:click={togglePath} class="logo" alt="vibia" />
 
-<div class="holder"> 
-    <img src={path} on:click={togglePath} class="logo" alt="vibia">
-    <div class="container element--border--primary">
-        <p>You are logged in as:</p>
-        <p>{$user.email}</p>
-    </div>
+	<div class="middle">
+		<a href="/">
+			<h1>Vibia's Market</h1>
+		</a>
+		{#if $user}
+			<p>{$user.email}</p>
+		{:else}
+			<p>&nbsp;</p>
+		{/if}
+	</div>
 
-    <div class="middle">
-        <a href="/">
-            <h1>Vibia's Market</h1>
-        </a>
-        {#if $user}
-            <p>{$user.email}</p>
-        {:else}
-            <p>&nbsp;</p>
-        {/if}
-    </div>
-
-
-    {#if $user}
-        <Secondary text="Sign Out" func={() => auth.signOut()}/>
-    {/if}
-    
-
+	{#if $user}
+		<Secondary text="Sign Out" func={() => auth.signOut()} />
+	{/if}
 </div>
 
-
-
 <style>
-    .container {
-        padding: var(--med-space);
-        display: flex;
-        align-items: flex-start;
-        justify-content: center;
-        flex-direction: column;
+	.container {
+		padding: var(--med-space);
+		display: flex;
+		align-items: flex-start;
+		justify-content: center;
+		flex-direction: column;
 
-        background: #ae823b;
-        color: #efeeec;
-        display: none;
-    }
+		background: #ae823b;
+		color: #efeeec;
+		display: none;
+	}
 
-    .holder {
-        display: flex;
-        gap: var(--med-space);
-        
-    }
+	.holder {
+		display: flex;
+		gap: var(--med-space);
+	}
 
-    .middle {
-        flex-grow: 1;
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        justify-content: center;
-    }
-    
-    .logo {
-        max-height: 7rem;
-    }
- 
-    .logout {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        padding: var(--med-space);
-        background-color: #8c869b;
-        color: #efeeec;
-    }
-    
+	.middle {
+		flex-grow: 1;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	.logo {
+		max-height: 7rem;
+	}
+
+	.logout {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: var(--med-space);
+		background-color: #8c869b;
+		color: #efeeec;
+	}
 </style>
-
-
-
