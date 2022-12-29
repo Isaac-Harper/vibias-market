88e52448b666ecb63d28866cb273df39133377e6
Isaac Harper <89951169+Voldari@users.noreply.github.com>
2022-12-29 00:08:48 -0700

    landing pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,7 +35,7 @@
 	{/if}
 
 	<div class="card">
-		<p>Virtual markets that make it easy to run shopping as a GM in your TTRPG if choice.</p>
+		<p>Welcome to Vibia's Market, the ultimate tool for making shopping in DnD and other role playing games easier than ever before! With Vibia's Market, you can create your own virtual market filled with all sorts of shops and items, inviting your players to shop to their heart's content. No more rummaging through endless pages of lists or trying to remember what you have in your inventory - Vibia's Market keeps it all organized and at your fingertips. So why wait? Start building your market today and take your role playing experience to the next level!</p>
 		<img alt="fishies" class="img" src="/images/image11.png" />
 	</div>
 
