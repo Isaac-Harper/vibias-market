c96822f55fbd282b882a467cb6679f06fae52f13
voldari <isaacharper@gmail.com>
2023-01-17 09:49:49 -0700

    mobile push

--- src/lib/MARKETS/MarketInfo.svelte
+++ src/lib/MARKETS/MarketInfo.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { current_patron } from './../Backend/state.ts';
 	import Primary from '$lib/Buttons/Primary.svelte';
 	import {
 		shop_list,
@@ -8,12 +9,13 @@
 		openMarketEdit,
 		resetShop
 	} from '$lib/Backend/state';
-	import { user } from '$lib/db';
+	import { createPatron, user } from '$lib/db';
 	import ShopCreation from '../SHOPS/ShopCreation.svelte';
 	import Inventory from '$lib/MARKETS/Inventory.svelte';
 	import { blur } from 'svelte/transition';
 	import MarketSettings from './MarketSettings.svelte';
 	import HorizontalList from '$lib/MARKETS/HorizontalList.svelte';
+	import TextInput from '$lib/TextInput.svelte';
 
 	$: current_shops = $shop_list.filter((v) => v.market_id === $current_market.id);
 
@@ -21,6 +23,8 @@
 		openShopCreate();
 		resetShop();
 	}
+
+	let new_name = '';
 
 	function openMarketSettings() {
 		openMarketEdit();
@@ -35,7 +39,16 @@
 			<p>{$current_market.description}</p>
 
 			<HorizontalList content={current_shops} list="shop" />
-			<Inventory />
+
+			{#if $current_patron.id === 0}
+				<div class="item">
+					<p>You don't have a character yet!</p>
+					<TextInput title="name" bind:value={new_name} />
+					<button on:click={() => createPatron(new_name)}>Create</button>
+				</div>
+			{:else}
+				<Inventory />
+			{/if}
 
 			{#if $user.id === $current_market.creator_id}
 				<div class="flex">
@@ -49,3 +62,13 @@
 		<MarketSettings />
 	</div>
 {/if}
+
+<style>
+	.item {
+		background-color: var(--inventory-background);
+		border: 2px solid var(--inventory-border);
+		border-radius: 1rem;
+		box-shadow: var(--inventory-border) 4px 4px 0 0;
+		padding: 1rem;
+	}
+</style>
